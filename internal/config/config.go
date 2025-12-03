db5ce6b5c870b4c75c0b381b59b23f5bf3e327ff
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-12-03 19:06:01 +0000

    Revert "update: require source hash"
    This reverts commit ded3502857901d7fca60be227318b5e31189c32f.

--- internal/config/config.go
+++ internal/config/config.go
@@ -66,38 +66,35 @@ func applyDefaults(cfg *Config) {
 	}
 }
 
-var sha256Regex = regexp.MustCompile(`^sha256-[0-9a-fA-F]{64}$`)
-
 func validate(cfg *Config) error {
+	sha256Regex := regexp.MustCompile(`^sha256-[0-9a-fA-F]{64}$`)
+
 	if cfg.Sandboxes != nil {
 		for name, sandbox := range cfg.Sandboxes {
-			if sandbox.Hash == "" {
-				return fmt.Errorf("sandbox '%s' requires a hash", name)
-			}
-			if !sha256Regex.MatchString(sandbox.Hash) {
-				return fmt.Errorf("sandbox '%s' has an invalid hash format: %s", name, sandbox.Hash)
+			if sandbox.Hash != nil {
+				if !sha256Regex.MatchString(*sandbox.Hash) {
+					return fmt.Errorf("sandbox '%s' has an invalid hash format: %s", name, *sandbox.Hash)
+				}
 			}
 		}
 	}
 
 	if cfg.Interceptors != nil {
 		for name, interceptor := range cfg.Interceptors {
-			if interceptor.Hash == "" {
-				return fmt.Errorf("interceptor '%s' requires a hash", name)
-			}
-			if !sha256Regex.MatchString(interceptor.Hash) {
-				return fmt.Errorf("interceptor '%s' has an invalid hash format: %s", name, interceptor.Hash)
+			if interceptor.Hash != nil {
+				if !sha256Regex.MatchString(*interceptor.Hash) {
+					return fmt.Errorf("interceptor '%s' has an invalid hash format: %s", name, *interceptor.Hash)
+				}
 			}
 		}
 	}
 
 	if cfg.Services != nil {
 		for name, service := range cfg.Services {
-			if service.Hash == "" {
-				return fmt.Errorf("service '%s' requires a hash", name)
-			}
-			if !sha256Regex.MatchString(service.Hash) {
-				return fmt.Errorf("service '%s' has an invalid hash format: %s", name, service.Hash)
+			if service.Hash != nil {
+				if !sha256Regex.MatchString(*service.Hash) {
+					return fmt.Errorf("service '%s' has an invalid hash format: %s", name, *service.Hash)
+				}
 			}
 		}
 	}
